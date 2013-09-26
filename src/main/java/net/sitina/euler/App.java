3c18bcc8d82dc60980e7d467dee32eeea1f68d7e
Jiri Sitina <jiri.sitina@gooddata.com>
2013-09-26 17:00:37 +0200

    Initial commit

---
+++ src/main/java/net/sitina/euler/App.java
@@ -0,0 +1,13 @@
+package net.sitina.euler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
